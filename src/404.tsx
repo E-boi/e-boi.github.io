5786545580d174cfc39bb4bb93c6937c28701956
e-boi <hiboysismyoufake6789@gmail.com>
2021-10-24 18:32:42 -0400

    because why not

--- src/404.tsx
+++ src/404.tsx
@@ -1,8 +1,10 @@
 import { Attributes } from 'preact';
+import icon from './profile.jpg';
 import './css/404.css';
 
 export default (_: Attributes) => (
 	<main className='lost'>
+		<img src={icon} height={64} />
 		<p>You lost?</p>
 	</main>
 );
